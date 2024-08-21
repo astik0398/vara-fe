8c7b46ac57b0ca406923ad580fe0598b8b9c053b
Kumar Astik <kumarastik0398@gmail.com>
2024-08-21 13:49:52 +0530

    updated the table UI and improved the overall UI

--- src/components/Csvcomponent.jsx
+++ src/components/Csvcomponent.jsx
@@ -2,31 +2,6 @@ import React, { CSSProperties, useState } from 'react';
 import { useCSVReader } from 'react-papaparse';
 import TableComponent from './TableComponent';
 
-const styles = {
-  csvReader: {
-    display: 'flex',
-    flexDirection: 'row',
-    marginBottom: 10,
-  },
-  browseFile: {
-    width: '20%',
-  },
-  acceptedFile: {
-    border: '1px solid #ccc',
-    height: 45,
-    lineHeight: 2.5,
-    paddingLeft: 10,
-    width: '80%',
-  },
-  remove: {
-    borderRadius: 0,
-    padding: '0 20px',
-  },
-  progressBarBackgroundColor: {
-    backgroundColor: 'red',
-  },
-};
-
 
 function Csvcomponent() {
 
@@ -35,6 +10,7 @@ function Csvcomponent() {
 
   return (
     <>
+    <div className='flex justify-center items-center mt-4'>
     <CSVReader
       onUploadAccepted={(results) => {
         setAllData(results)
@@ -47,24 +23,24 @@ function Csvcomponent() {
         getRemoveFileProps,
       }) => (
         <>
-          <div style={styles.csvReader}>
-            <button type='button' {...getRootProps()} style={styles.browseFile}>
+          <div >
+            <button className='text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700' type='button' {...getRootProps()}>
               Browse file
             </button>
-            <div style={styles.acceptedFile}>
+            <div style={{textAlign:'center'}}>
               {acceptedFile && acceptedFile.name}
             </div>
-            <button {...getRemoveFileProps()} style={styles.remove}>
-              Remove
-            </button>
           </div>
-          <ProgressBar style={styles.progressBarBackgroundColor} />
+          <ProgressBar />
         </>
       )}
     </CSVReader>
-
+    </div>
+    <div style={{textAlign:'center', marginTop:'20px'}}>
     <TableComponent data={allData}/>
+    </div>
     </>
+    
   );
 
 }
--- src/components/TableComponent.jsx
+++ src/components/TableComponent.jsx
@@ -1,29 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TableComponent = ({data}) => {
 
+  const [columnSelect, setColumnSelect] = useState(null)
+  const [rowSelect, setRowSelect] = useState(null)
+
     if (!Array.isArray(data.data) || data.data.length === 0) {
-        return <h2>No data to display!</h2>;
-      }
+        return <h2 className='text-2xl'>No data to display! Please upload a CSV file!</h2>;
+    }
 
     const header = data.data[0]
     const rows = data.data.slice(1)
+
+    function handleColumnSel(ind){
+      setColumnSelect(ind)
+    }
+
+    function handleRowSel(ind){
+      setRowSelect(ind)
+    }
     
   return (
-    <div>
-        <table>
-        <thead>
+    <div >
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400" style={{border:'1px solid #a1a2a4', margin:'auto'}}>
+        <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
           <tr>
             {header.map((header, index) => (
-              <th key={index}>{header}</th>
+              <th onClick={()=> handleColumnSel(index)} scope="col" className={`hover:bg-gray-500 hover:text-white cursor-pointer px-6 py-3 ${columnSelect==index ? 'bg-gray-500 text-white' : ''}`} key={index}>{header}</th>
             ))}
           </tr>
         </thead>
         <tbody>
           {rows.map((row, rowIndex) => (
-            <tr key={rowIndex}>
+            <tr onClick={()=> handleRowSel(rowIndex)} className={`bg-white border-b dark:bg-gray-800 dark:border-gray-700 ${rowSelect == rowIndex ? 'bg-pink-400 text-white' : ''}`} key={rowIndex}>
               {row.map((cell, cellIndex) => (
-                <td key={cellIndex}>{cell}</td>
+                <td className={`cursor-pointer px-6 py-4 ${columnSelect == cellIndex ? 'bg-yellow-500 text-white' : ''}`} key={cellIndex}>{cell}</td>
               ))}
             </tr>
           ))}
